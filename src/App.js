33130efe366552b18c006e037cceedf73d2e55e4
E.Yallameha <yallameh@gmail.com>
2021-11-21 13:50:53 +0330

    Init

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Yall!
+          Learn React Yall!! __
         </a>
       </header>
     </div>
